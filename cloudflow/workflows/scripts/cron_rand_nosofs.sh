104998d60a59ac6d96703df52a275e9bba27323f
Patrick Tripp <patrick.tripp@rpsgroup.com>
2020-09-01 15:33:54 -0400

    implement random ofs for cron

---
+++ cloudflow/workflows/scripts/cron_rand_nosofs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#__copyright__ = "Copyright © 2020 RPS Group, Inc. All rights reserved."
+#__license__ = "See LICENSE.txt"
+#__email__ = "patrick.tripp@rpsgroup.com"
+
+# Crontab entry example
+#30  9   *   *   Thu  nohup /usr/bin/bash -lc $HOME/Cloud-Sandbox/workflow/cron_liveocean.sh < /dev/null > $HOME/locron.out 2>&1
+
+CFHOME=$HOME/Cloud-Sandbox/cloudflow
+cd $CFHOME
+
+rand=`$CFHOME/workflows/scripts/random_ofs.py`
+ofs=`echo $rand | awk '{print $1}'`
+cyc=`echo $rand | awk '{print $2}'`
+
+echo "$ofs $cyc"
+
+fcst=job/jobs/${ofs}.${cyc}z.fcst
+plots=job/jobs/${ofs}.${cyc}z.diffplots
+
+nohup ./workflows/nosofs_qops.py $fcst $plots > $HOME/${ofs}.log 2>&1
+
+#nohup ./workflows/nosofs_qops.py job/jobs/dbofs.00z.fcst < /dev/null > $HOME/dbofs.log &
