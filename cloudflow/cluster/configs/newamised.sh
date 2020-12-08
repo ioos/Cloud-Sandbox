18ecf6d84f13dc5b2f5001cbe5e53715eeb3ef0f
Patrick Tripp <patrick.tripp@rpsgroup.com>
2020-12-08 15:21:57 -0500

    added user - updated ami

--- cloudflow/cluster/configs/newamised.sh
+++ cloudflow/cluster/configs/newamised.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-newami='ami-0b1f32f8427ff563e'
-oldami='ami-0da0626fd586c935c'
+#newami='ami-0b1f32f8427ff563e'
+#oldami='ami-0da0626fd586c935c'
 
+oldami='ami-0b1f32f8427ff563e'
+newami='ami-0f3150c0c20b4cf29'
 
-#for file in `ls -1 *ofs.config`
 for file in `ls -1 *.config`
 do
   echo $file
