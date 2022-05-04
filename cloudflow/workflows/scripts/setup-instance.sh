6bb1601a040c7bb535c0778e8c9e206abcada09c
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2022-05-04 13:55:11 +0000

    not using spack for munge and slurm - testing

--- cloudflow/workflows/scripts/setup-instance.sh
+++ cloudflow/workflows/scripts/setup-instance.sh
@@ -9,22 +9,24 @@
 sudo setenforce 0
 
 # DO NOT CHANGE THE ORDER!
-setup_environment
-setup_paths
-setup_aliases
-install_efa_driver
-install_python_modules_user
+#setup_environment
+#setup_paths
+#setup_aliases
+#install_efa_driver
+#install_python_modules_user
 
-install_spack
-install_gcc
-install_intel_oneapi
-install_netcdf
-#install_hdf5
-install_slurm
-install_esmf
-install_base_rpms
-install_extra_rpms
-install_ffmpeg
+#install_spack
+#install_gcc
+#install_intel_oneapi
+#install_netcdf
+install_hdf5
+
+#install_munge
+#install_slurm
+#install_esmf
+#install_base_rpms
+# install_extra_rpms
+#install_ffmpeg
 
 sudo yum -y clean all
 
