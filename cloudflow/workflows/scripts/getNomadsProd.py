ef653a0e3938291fbfcb1862945d3a47a203c6cc
JohnSpeers <John.Speers@rpsgroup.com>
2022-07-25 16:15:09 -0400

    Updated getNomadsProd script

--- cloudflow/workflows/scripts/getNomadsProd.py
+++ cloudflow/workflows/scripts/getNomadsProd.py
@@ -1,7 +1,7 @@
 import os
 import urllib.request
 
-def getICsFVCOM(ofs, cdate, hh, vdir):
+def getNomadsProd(ofs, cdate, cyc, vdir):
     if len(locals()) < 3:
         print('Usage: {} cbofs|ngofs|etc. yyyymmdd hh [/com/nos-noaa/cbofs.20200416 | other destination]'.format(ofs))
         exit()
@@ -9,12 +9,83 @@ def getICsFVCOM(ofs, cdate, hh, vdir):
     if len(locals()) > 3:
         dest = vdir
     else:
-        dest='{}.{}'.format(ofs,cdate)
+        dest = '{}.{}'.format(ofs,cdate)
 
     err = 0
     
     os.makedirs(dest)
     os.chdir(dest)
+    
+    nomads='https://nomads.ncep.noaa.gov/pub/data/nccf/com/nos/prod/{}.{}'.format(ofs,cdate)
 
-    
-    
+    # Download every hour forecast
+    ###############################################################
+    # Forecasts don't follow the same pattern, some don't have f000, some are 3 hourly (gomofs)
+    hlist=['00','01','02','03','04','05','06','07','08','09']
+
+    # Most forecasts are 48 hours, with some exceptions
+    ehr=48
+    if ofs == 'gomofs':
+        ehr = 72
+    elif ofs == 'ngofs':
+        ehr = 54
+    elif ofs == 'leofs' or ofs == 'lmhofs':
+        ehr = 120
+
+    for hh in hlist:
+        hhFile = 'nos.{}.fields.f0{}.{}.t{}z.nc'.format(ofs,hh,cdate,cyc)
+        try:
+            urllib.request.urlretrieve('{}/{}'.format(nomads,hhFile))
+        except:
+            print('ERROR: Unable to retrieve {} from \n {}'.format(hhFile,nomads))
+            exit()
+
+    hh = 10
+    while hh < ehr:
+        if hh < 100:
+            hhstr = '0{}'.format(hh)
+        else:
+            hhstr = str(hh)
+        file = 'nos.{}.fields.f{}.{}.t{}z.nc'.format(ofs,hhstr,cdate,cyc)
+        try:
+            urllib.request.urlretrieve('{}/{}'.format(nomads,file))
+        except:
+            print('ERROR: Unable to retrieve {} from \n {}'.format(file,nomads))
+            exit()
+        hh+=1
+    ###############################################################
+
+    # Get the nestnode files if ngofs 
+    # Not neccessary for Ngofs2??
+
+    #if ofs == 'ngofs' or ofs == 'ngofs2':
+    #    file1 = 'nos.{}.obc.{}.t{}z.nc'.format(ofs,cdate,cyc)
+    #    try:
+    #        urllib.request.urlretrieve('{}/{}'.format(nomads,file1))
+    #    except:
+    #        exit()
+
+    #   os.rename(file1, 'nos.{}.nestnode.forecast.{}.t{}z.nc'.format(ofs,cdate,cyc))
+    #    
+    #    file2 = 'nos.{}.obc.{}.t{}z.nc'.format(ofs,cdate,cyc)
+    #    try:
+    #        urllib.request.urlretrieve('{}/{}'.format(nomads,file2))
+    #    except:
+    #        exit()
+
+    # Get the log and .in config files
+    inFile = 'nos.{}.forecast.{}.t{}z.in'.format(ofs,cdate,cyc)
+    logFile = 'nos.{}.forecast.{}.t{}z.log'.format(ofs,cdate,cyc)
+    try:
+        urllib.request.urlretrieve('{}/{}'.format(nomads,inFile))
+    except:
+        print('ERROR: Unable to retrieve {} from \n {}'.format(inFile,nomads))
+        exit()
+    try:
+        urllib.request.urlretrieve('{}/{}'.format(nomads,logFile))
+    except:
+        print('ERROR: Unable to retrieve {} from \n {}'.format(logFile,nomads))
+        exit()
+
+if __name__ == '__main__':
+    getNomadsProd('leofs', '20220725', '00', '../../../../test_file_store/nomads_leofs_py/')
