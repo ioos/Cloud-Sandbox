e71ac949ccf6c1a2741de74aa30b8492aeafbc70
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2023-03-24 12:04:36 -0400

    Nyh updates (#43)
    * added support for nyh-hindcast
    
    * fix echo of command

--- cloudflow/workflows/fcst_launcher.sh
+++ cloudflow/workflows/fcst_launcher.sh
@@ -28,12 +28,12 @@ fi
 
 export CDATE=$1
 export HH=$2
-export COMOUT=$3
+export COMOUT=$3   # OUTDIR in caller
 export NPROCS=$4
 export PPN=$5
 export HOSTS=$6
 export OFS=$7
-export EXEC=$8       # only used for ADNOC currently
+export EXEC=$8
 
 #OpenMPI
 #mpirun --version
@@ -45,16 +45,20 @@ export EXEC=$8       # only used for ADNOC currently
 #Copyright (C) 2003-2017, Intel Corporation. All rights reserved.
 
 # module -t avail >& avail ; grep mpi avail
-# mpirun not in path if module is not loaded
-#mpirun --version | grep Intel
-#impi=$?
+
+# TODO: put the following back in
+# mpirun --version | grep Intel
+# impi=$?
 #
-#mpirun --version | grep "Open MPI"
-#openmpi=$?
+# mpirun --version | grep "Open MPI"
+# openmpi=$?
 
 # for openMPI openmpi=1
 # for Intel MPI set impi=1
 if [[ $OFS == "adnoc" ]]; then
+  openmpi=1
+  impi=0
+elif [[ $OFS == "nyh-hindcast" ]]; then
   openmpi=1
   impi=0
 else
@@ -117,6 +121,13 @@ case $OFS in
     mpirun $MPIOPTS $EXEC ocean.in > ocean.log
     result=$?
     ;;
+  nyh-hindcast)
+    EXEC=${EXEC:-roms}
+    export JOBDIR=$COMOUT
+    mkdir -p $JOBDIR
+    cd $JOBDIR || exit 1
+    echo "Run command: mpirun $MPIOPTS $EXEC ocean.in > ocean.out 2>&1"
+    mpirun $MPIOPTS $EXEC ocean.in > ocean.out 2>&1
   adcircofs)
     #export HOMEnos=/save/ADCIRC_Copied/
     export JOBDIR=/home/mmonim/Cloud-Sandbox/cloudflow/workflows
