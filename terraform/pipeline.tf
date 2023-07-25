bce85a4aef411ebf40223cae2aa70a72774c00ce
ZacharyWills <60660437+ZacharyWills@users.noreply.github.com>
2023-07-24 18:43:06 -0600

    Update pipeline.tf to change account numbers and fix some typos

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -94,7 +94,7 @@ resource "aws_imagebuilder_image_pipeline" "headnode" {
 
   schedule {
     schedule_expression = "cron(0 8 ? * tue)"
-    # This cron expressions states every Tuesday at 8 AM.
+    # This cron expression states every Tuesday at 8 AM.
     pipeline_execution_start_condition = "EXPRESSION_MATCH_AND_DEPENDENCY_UPDATES_AVAILABLE"
   }
 
@@ -105,7 +105,7 @@ resource "aws_imagebuilder_image_pipeline" "headnode" {
   }
 
   tags = {
-    "Name" = "${var.ami_name_tag}-pipeline"
+    "Name" = "headnode-pipeline"
   }
 }
 resource "aws_imagebuilder_image" "headnode" {
@@ -151,17 +151,17 @@ data "aws_kms_key" "image_builder" {
 }
 
 # Amazon Cloudwatch agent component
-resource "aws_imagebuilder_component" "cw_agent" {
-  name       = "amazon-cloudwatch-agent-linux"
-  platform   = "Linux"
-  uri        = "s3://${var.aws_s3_bucket_object}/files/amazon-cloudwatch-agent-linux.yml"
-  version    = "1.0.0"
-  kms_key_id = data.aws_kms_key.image_builder.arn
-
-  depends_on = [
-    aws_s3_bucket_object.cw_agent_upload
-  ]
-}
+# resource "aws_imagebuilder_component" "cw_agent" {
+#  name       = "amazon-cloudwatch-agent-linux"
+#  platform   = "Linux"
+#  uri        = "s3://${var.aws_s3_bucket_object}/files/amazon-cloudwatch-agent-linux.yml"
+#  version    = "1.0.0"
+#  kms_key_id = data.aws_kms_key.image_builder.arn
+#
+#  depends_on = [
+#    aws_s3_bucket_object.cw_agent_upload
+#  ]
+#}
 resource "aws_imagebuilder_infrastructure_configuration" "headnode" {
   description           = "Simple infrastructure configuration"
   instance_profile_name = var.ec2_iam_role_name
@@ -196,7 +196,7 @@ resource "aws_imagebuilder_distribution_configuration" "headnode" {
       name = "amzn-linux-{{ imagebuilder:buildDate }}"
 
       launch_permission {
-        user_ids = ["123456789012"]
+        user_ids = ["309956199498"]
       }
     }
     region = var.aws_region
