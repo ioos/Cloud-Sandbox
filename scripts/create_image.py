b7349394b59468bca07046ac50bc074c8329a547
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2023-12-12 09:59:33 -0500

    image creation timouts

--- scripts/create_image.py
+++ scripts/create_image.py
@@ -26,7 +26,11 @@ def main():
 
     
     snapshot_id = create_snapshot(instance_id, image_name, project_tag)
-    #print(f"DEBUG: snapshot_id: {snapshot_id}")
+    if snapshot_id == None:
+      print("ERROR: could not create snapshot")
+      sys.exit(1)
+
+    print(f"snapshot_id: {snapshot_id}")
 
     image_id  = create_image_from_snapshot(snapshot_id, image_name)
     print(str(image_id))
@@ -36,7 +40,6 @@ def create_snapshot(instance_id: str, name_tag: str, project_tag: str):
   ''' NOTE: Run 'sync' on filesystem to flush the disk cache before running this ''' 
 
   # print(f"create_snapshot: instance_id: {instance_id}, name_tag: {name_tag}, project_tag: {project_tag}")
-
 
   ec2 = boto3.client('ec2', region_name='us-east-2')
 
@@ -96,7 +99,7 @@ def create_image_from_snapshot(snapshot_id: str, image_name: str):
 
   # Wait for snapshot to be created 
   # wait_until will throw an exception after 10 minutes
-  maxtries=3
+  maxtries=5
   tries=0
   while tries < maxtries:
     try:
