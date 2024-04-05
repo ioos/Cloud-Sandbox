1be3093a885cf237f6c991bd1a8e0b664c1f6c27
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2024-04-05 14:21:53 -0400

    LiveOcean multi-day hindcasts x4b (#81)
    * LO_hindcast
    
    * updated templates for shorter timestep
    
    * moved setup.log

--- cloudflow/workflows/lohindcast_launch.py
+++ cloudflow/workflows/lohindcast_launch.py
@@ -27,6 +27,7 @@ fcstconf = f'{curdir}/../cluster/configs/liveocean.config'
 
 # This is used for obtaining liveocean forcing data
 # LiveOcean users need to obtain credentials from UW
+
 sshuser = 'mlalime@apogee.ocean.washington.edu'
 #sshuser = 'ptripp@apogee.ocean.washington.edu'
 
