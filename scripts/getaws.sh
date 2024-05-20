256691be86d42c47c86dffef04c99782fac19347
ZacharyWills <60660437+ZacharyWills@users.noreply.github.com>
2024-05-20 14:58:25 -0600

    Update getaws.sh to add SSM Agent

--- scripts/getaws.sh
+++ scripts/getaws.sh
@@ -3,3 +3,8 @@
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+#Add SSM agent for AWS
+sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm 
+sudo systemctl enable amazon-ssm-agent 
+sudo systemctl start amazon-ssm-agent
