d47cea5af8c7cb41ae7e65dcb1399ff18c033b34
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2024-06-03 14:30:03 +0000

    Minor fixes

--- cloudflow/cluster/nodeInfo.py
+++ cloudflow/cluster/nodeInfo.py
@@ -5,7 +5,6 @@ __copyright__ = "Copyright © 2023 RPS Group, Inc. All rights reserved."
 __license__ = "BSD 3-Clause"
 
 
-
 awsTypes = {'c5.large': 1, 'c5.xlarge': 2, 'c5.2xlarge': 4, 'c5.4xlarge': 8, 'c5.9xlarge': 18, 'c5.12xlarge': 24,
             'c5.18xlarge': 36, 'c5.24xlarge': 48, 'c5.metal': 36,
             'c5n.large': 1, 'c5n.xlarge': 2, 'c5n.2xlarge': 4, 'c5n.4xlarge': 8, 'c5n.9xlarge': 18,
@@ -13,8 +12,6 @@ awsTypes = {'c5.large': 1, 'c5.xlarge': 2, 'c5.2xlarge': 4, 'c5.4xlarge': 8, 'c5
             't3.large': 1, 't3.xlarge': 2, 't3.2xlarge': 4,
             'c5a.2xlarge': 4, 'c5a.4xlarge': 8, 'c5a.24xlarge': 48,
             'hpc6a.48xlarge': 96, 'hpc7a.96xlarge': 192, 'hpc7a.48xlarge': 96, 'c7i.48xlarge': 96, 'r7iz.32xlarge': 64 }
-
-efaTypes = ['c5n.18xlarge', 'hpc6a.48xlarge', 'hpc7a.96xlarge', 'hpc7a.48xlarge']
 
 
 def getPPN(instance_type):
@@ -32,16 +29,6 @@ def getPPN(instance_type):
     ------
     Exception if instance_type is not found
     """
-
-    # awsTypes = {'c5.large': 1, 'c5.xlarge': 2, 'c5.2xlarge': 4, 'c5.4xlarge': 8, 'c5.9xlarge': 18, 'c5.12xlarge': 24,
-    #             'c5.18xlarge': 36, 'c5.24xlarge': 48, 'c5.metal': 36, 
-    #             'c5n.large': 1, 'c5n.xlarge': 2, 'c5n.2xlarge': 4, 'c5n.4xlarge': 8, 'c5n.9xlarge': 18, 
-    #             'c5n.18xlarge': 36, 'c5n.24xlarge': 48, 'c5n.metal': 36,
-    #             't3.large': 1, 't3.xlarge': 2, 't3.2xlarge': 4, 
-    #             'c5a.2xlarge': 4, 'c5a.4xlarge': 8, 'c5a.24xlarge': 48,
-    #             'hpc6a.48xlarge': 96 }
-
-    # efaTypes = ['c5n.18xlarge', 'hpc6a.48xlarge']
 
     try:
         ppn = awsTypes[instance_type]
