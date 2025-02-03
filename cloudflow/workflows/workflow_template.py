4cbc6e6d559f6ac6c5704e98b8a5b2935c13384b
Cloud User <ec2-user@ip-10-26-37-40.us-east-2.compute.internal>
2025-02-03 18:14:08 +0000

    Add new workflow file that will be used as a template for modelers to include and run their models on the Cloud-Sandbox

---
+++ cloudflow/workflows/workflow_template.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env -S python3 -u
+""" A driver to run a simplle hindcast workflow from provided job configuration files """
+
+''' Usage:
+
+    ./workflows/workflow_main.py hindcast-job
+
+    Examples: ./workflows/workflow_main.py job/jobs/schism.hindcast
+
+'''
+
+import collections
+import os
+import sys
+import re
+from signal import signal, SIGINT
+
+if os.path.abspath('..') not in sys.path:
+    sys.path.append(os.path.abspath('..'))
+
+from cloudflow.utils import romsUtil as util
+from cloudflow.workflows import flows
+
+__copyright__ = "Copyright © 2023 RPS Group, Inc. All rights reserved."
+__license__ = "BSD 3-Clause"
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+
+######################################################################
+# Set you AWS cloud configuration file here for your given
+# model deployment on the cloud environment
+######################################################################
+
+template_conf = f'{curdir}/../cluster/configs/dflowfm.ioos'
+
+######################################################################
+
+def handler(signal_received, frame):
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    raise signal.FAIL()
+
+
+## The template workflow driver script for any model run in Cloud-Sandbox ##
+def main():
+
+    signal(SIGINT, handler)
+
+    # Read in user defined job file
+    jobfile = os.path.abspath(sys.argv[1])
+
+    # Initialize collections item for flow object
+    flowdeq = collections.deque()
+
+    # Reads in job configuration file to inform
+    # user of the job type they defined and are
+    # running in this template workflow
+    jobdict = util.readConfig(jobfile)
+    jobtype = jobdict["JOBTYPE"]
+    print('JOBTYPE: ', jobtype)
+
+
+    simple_flow = flows.template_flow(template_conf, jobfile)
+    flowdeq.appendleft(simple_flow)
+
+    qlen = len(flowdeq)
+    idx = 0
+
+
+    # Run all of the flows in the queue
+    while idx < qlen:
+        aflow = flowdeq.pop()
+        idx += 1
+
+        # Stop if the flow failed
+        state = aflow.run()
+        if state.is_successful():
+            continue
+        else:
+            break
+
+#####################################################################
+
+
+if __name__ == '__main__':
+    main()
