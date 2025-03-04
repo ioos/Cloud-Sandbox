fa701f7cad9e1695a31ebfc91960c15897874038
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2025-03-04 20:39:44 +0000

    run_storms.sh NWS 14 section

---
+++ models/adcirc-cora/bin/mods.sh
@@ -0,0 +1,8 @@
+module purge
+module load icc/2022.0.2
+module load env_icc/any
+module load hdf5/1.12.1-intel
+module load netcdf-c/4.8.1-intel
+module load netcdf-fortran/4.5.4-intel
+module load mvapich2/2.3.7-intel
+#module load nco
