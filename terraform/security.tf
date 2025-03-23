e5a81f3471b9657863dd547e40d2e2c0d667cf94
Your Name <your-email@example.com>
2025-03-23 22:02:44 +0530

    Fixed the bug

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,17 +1,17 @@
-# base sg
+# Base SG - Allows all traffic within the same security group
 resource "aws_security_group" "base_sg" {
   vpc_id = local.vpc.id
   ingress {
-    self      = true
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    self        = true
   }
   egress {
-    self      = true
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name    = "${var.name_tag} Base SG"
@@ -19,28 +19,28 @@ resource "aws_security_group" "base_sg" {
   }
 }
 
+# EFS SG - Allows NFS traffic only from base_sg
 resource "aws_security_group" "efs_sg" {
-   vpc_id = local.vpc.id
-   ingress {
-     self = true
-     from_port = 2049
-     to_port = 2049
-     protocol = "tcp"
-   }
-   # allow all outgoing from NFS
-   egress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
-   }
-   
-   tags = {
-      Name = "${var.name_tag} EFS SG"
-      Project = var.project_tag
-   }
+  vpc_id = local.vpc.id
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.base_sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "${var.name_tag} EFS SG"
+    Project = var.project_tag
+  }
 }
 
+# SSH SG - Allows SSH only from approved IPs
 resource "aws_security_group" "ssh_ingress" {
   vpc_id = local.vpc.id
   ingress {
@@ -50,8 +50,15 @@ resource "aws_security_group" "ssh_ingress" {
     protocol    = "tcp"
     cidr_blocks = var.allowed_ssh_cidr_list
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
-      Name = "${var.name_tag} SSH SG"
-      Project = var.project_tag
-   }
+    Name    = "${var.name_tag} SSH SG"
+    Project = var.project_tag
+  }
 }
+
