763d40dba4480adf99e1bff19533b60c6a892d27
Mykel Alvis <mykel.alvis@gmail.com>
2025-04-24 12:21:40 -0500

    Add packages for scisa

--- skill_systems/user_data.sh
+++ skill_systems/user_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# TODO All of this should be migrated to an ansible playbook
 sudo dnf -y update
 sudo dnf -y install python3.12
 sudo dnf -y install https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
@@ -9,3 +10,6 @@ sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-cloudwatch-agent
 sudo systemctl start amazon-cloudwatch-agent
 sudo dnf -y install git
+sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf -y install neovim python3-neovim
+sudo dnf -y install screen
