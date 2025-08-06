daaef62f6d713be204fb041388b8a4f6377a39d8
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2025-08-06 16:41:29 +0000

    fixed necofs inputs download

--- models/necofs/hindcasts/get_testcase_inputs.sh
+++ models/necofs/hindcasts/get_testcase_inputs.sh
@@ -9,6 +9,9 @@ set -e
 SBUSER=$USER
 INSTALLDIR=/com/$SBUSER/necofs/NECOFS_v2.4
 
+BUCKETPRE=ioos-sandbox-use2/public/necofs
+FILE=necofs.20190101.input.tgz
+
 echo "Installing in: $INSTALLDIR"
 
 if [ ! -d $INSTALLDIR ]; then
@@ -19,13 +22,13 @@ cd $INSTALLDIR || exit 1
 
 # Only fetch it if it doesn't already exist
 if [ ! -d hindcasts/201901 ]; then
-  aws s3 cp s3://ioos-sandbox-use2/public/necofs/hindcast.201901.input.tgz .
-  tar -xvf hindcast.201901.input.tgz
+  aws s3 cp s3://${BUCKETPRE}/${FILE} .
+  tar -xvf $FILE
 
   # Everything should now be in the hindcasts/201901 folder, ready to run the workflow
-  echo "Test case input files should now be available in $INSTALLDIR/hindcasts/201901"
-  rm hindcast.201901.input.tgz
+  echo "Test case input files should now be available in $INSTALLDIR/hindcasts/20190101"
+  rm $FILE
 else
-  echo "It looks like there is already data present for $INSTALLDIR/hindcasts/201901"
+  echo "It looks like there is already data present for $INSTALLDIR/hindcasts/20190101"
   echo "Not downloading the testcase data"
 fi
