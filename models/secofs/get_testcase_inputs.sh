0925e8dcf16b4b92560358686d0ba1647a47c196
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2025-08-15 21:33:34 +0000

    secofs and eccofs testing

--- models/secofs/get_testcase_inputs.sh
+++ models/secofs/get_testcase_inputs.sh
@@ -8,19 +8,21 @@ set -e
 # You must set this as needed for proper folders for your user if different from login user name/path
 SBUSER=$USER
 
+DATAFILE=secofs.20171201.inputs.tgz
+
 if [ ! -d /com/$SBUSER/secofs ]; then
     mkdir -p /com/$SBUSER/secofs
 fi
 
-cd /com/$SBUSER || exit 1
+cd /com/$SBUSER/secofs || exit 1
 
-aws s3 cp s3://ioos-sandbox-use2/public/secofs/secofs.20171201.inputs.tar .
-tar -xvf secofs.20171201.inputs.tar
+aws s3 cp s3://ioos-sandbox-use2/public/secofs/$DATAFILE .
+tar -xvf $DATAFILE
 
-# Extracts to secofs/20171201/inputs
+# Extracts to ./20171201
 
 # Everything should now be in the 20171201 folder, ready to run the workflow
 
-echo "Testcase data for 20171201 should now be in /com/$SBUSER/secofs/20171201/inputs"
+echo "Testcase data for 20171201 should now be in /com/$SBUSER/secofs/20171201"
 echo "Note: from VIMS RUN08j_JZ"
-rm secofs.20171201.inputs.tar
+rm $DATAFILE
