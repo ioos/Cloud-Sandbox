1d0a3887c0b6c27eccd0f8f076b1ec2d0162aad2
Jason Ducker <81377226+jduckerOWP@users.noreply.github.com>
2025-09-09 17:30:24 -0400

    AWS Cluster Update to Dynamically Retry to Obtain EC2 Instance Resources Not Currently Available in Region (#125)
    * Update CLOUD_SANDBOX_MODEL_INTEGRATION_TEMPLATE.md with Minna Ho's suggestions
    * Update workflow_main.py
    
    * Include OWP and NOS model suites as eligible job types for the workflow
    
    * Delete cloudflow/workflows/workflow_template.py
    
    * Eliminate original workflow template method for executing Cloudflow using the model templates provided for eligible OWP and NOS models. This will conform to the new updated instructions for integrating a new model template to Cloudflow.
    
    * Modify SCHISM OWP cluster configuration file to reflect updated changes to use hpc node instance suite
    
    * Update AWS Cluster start function to include user or default options to sleep and retry to inquire AWS resources if the server responds as InsufficentCapacity for the AWS region listed on the cluster configuration file
    
    * Update the default SCHISM job configuration file to include the new options NUM_TRIES and JOB_DELAY to utilize the new cloudflow method for waiting to obtain AWS resources based on the user patience to obtain them
    
    * Modify CLOUD_SANDBOX_MODEL_INTEGRATION_TEMPLATE.md to fix small syntax error with replacing the workflow_main.py Python executable with user specified pathway
    
    * Modify the template_launcher.sh shell script in order to remove MPI variables predefined in the launcher script and move them to the model launcher scripts. Also, schism is now predefined to partition the number of tasks associated with each node instance in order to optimize memory resources between OpenMP and MPI protocols for slave ranks. This is required in order for SCHISM to properly work on the hpc node instance suite for large SCHISM meshes
    
    * Modify all OWP, NOS, and default model template shell launcher scripts to use the lateste spack modules on the NOS Cloud Sandbox that has the newer intel libraries eligble to run on the hpc7a node instances
    
    * Add on new AWS Cluster cluster configuration options vm_retry_delay and vm_max_retries to workflow. Include as well an expotential backoff method in the job delay over each retry iteration. Update schism.ioos configuration template options to include new delay options for users.

--- cloudflow/workflows/model_template_run.sh
+++ cloudflow/workflows/model_template_run.sh
@@ -10,16 +10,19 @@ set -x       # verbose with command expansion
 set -u       # forces exit on undefined variables
 
 
+# Purge current modules loaded and use latest spack
+# module installations compatible with hpc7as
 module purge
-module use -a /mnt/efs/fs1/save/environments/spack/share/spack/modules/linux-rhel8-x86_64
+module use -a /mnt/efs/fs1/save/environments/spack.v0.22.5/share/spack/modules/linux-rhel8-x86_64
 
 # User needs to load up the defined compilers and libraries
 # they used for compiling their model within this block here
-module load intel-oneapi-compilers/2023.1.0-gcc-11.2.1-3a7dxu3
-module load intel-oneapi-mpi/2021.9.0-intel-2021.9.0-egjrbfg
-module load netcdf-c/4.9.2-intel-2021.9.0-vznmeik
-module load netcdf-fortran/4.6.1-intel-2021.9.0-meeveoj
-module load parallelio/2.6.2-intel-2021.9.0-csz55zr
+
+module load intel-oneapi-compilers/2023.1.0-gcc-11.2.1-3rbcwfi
+module load intel-oneapi-mpi/2021.12.1-intel-2021.9.0-6nra3z4
+module load hdf5/1.14.3-intel-2021.9.0-jjst2zs
+module load netcdf-c/4.9.2-intel-2021.9.0-vkckbzk
+module load netcdf-fortran/4.6.1-intel-2021.9.0-cpxxwci
 
 # Export required variables
 # for your model executable
@@ -27,6 +30,13 @@ module load parallelio/2.6.2-intel-2021.9.0-csz55zr
 export FC=mpiifort
 export CXX=mpiicpc
 export CC=mpiicc
+
+# Define the AWS MPI options to utilize
+# the efa fabric for hpc7as
+export I_MPI_OFI_LIBRARY_INTERNAL=0   # Using AWS EFA Fabric on AWS
+export FI_PROVIDER=efa
+export I_MPI_FABRICS=ofi
+export I_MPI_OFI_PROVIDER=efa
 
 # This is where you define your 
 # shell script required inputs
