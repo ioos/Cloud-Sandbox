3f551f1dd46336bba3a14aaf8d7e3a46f4a8d784
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2025-09-19 21:45:39 +0000

    readme and setup updates for nosofs

--- models/nosofs/setup_nosofs.sh
+++ models/nosofs/setup_nosofs.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 #   fetch the operational NOSOFS source codes
 #   build the models and other executables needed by the production suite
 #   retrieve the fixed fields needed to run the models
-
+#   
+# If you are using a shared "USER" account, make sure to
+# set the below path to your own SAVE space 
+# or provide it as a command argument
 
 export SAVEDIR=${1:-"/save/$USER"}
 if [ ! -d $SAVEDIR ]; then
--- models/secofs/fetch_vims_data.sh
+++ models/secofs/fetch_vims_data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Username and password is provided by VIMS
+
 USERNAME=$1
 if [[ $USERNAME == "" ]]; then
   echo "Usage $0 username"
@@ -12,9 +14,18 @@ cd $DATADIR || exit 1
 
 URL='https://ccrm.vims.edu/yinglong/NOAA/COOPS/RUN08j_JZ/'
 
-WGET_OPTS='--recursive --no-clobber --continue --no-parent --reject-regex "*\.html\?.*"'
+WGET_OPTS='--recursive --no-clobber --continue --no-parent --reject-regex ".*\.html.*"'
 RETRY='--tries=5 --random-wait --waitretry=5'
 TIMEOUT='--connect-timeout=5 --read-timeout=30'
 
 wget --user=$USERNAME --ask-password --no-check-certificate $WGET_OPTS $RETRY $TIMEOUT $URL
 
+mv ccrm.vims.edu/yinglong/NOAA/COOPS/RUN08j_JZ/ .
+rm -Rf ccrm.vims.edu/
+cd RUN08j_JZ | exit 1
+rm *index.html*
+rm sflux/*index.html*
+
+cd ..
+
+aws s3 sync RUN08j_JZ/ s3://ioos-sandbox-use2/secofs/input/RUN08j_JZ/
