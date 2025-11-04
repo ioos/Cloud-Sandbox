7d99d42fbc2deef39e806405d7bf208b2ec92bb4
Cloud User <ID+username@users.noreply.github.com>
2025-11-04 18:42:41 +0000

    Default the job type application to be basic for users if they do not specify it in the job configuration file. Also remove ucla_roms.py job class since it's been merged with the ROMS_Experiment class

--- cloudflow/job/WRF_Hydro_Experiment.py
+++ cloudflow/job/WRF_Hydro_Experiment.py
@@ -82,7 +82,7 @@ class WRF_Hydro_Experiment(Job):
 
         self.MODEL = cfDict['MODEL']
         self.jobtype = cfDict['JOBTYPE']
-        self.APP = cfDict.get('APP', "default")
+        self.APP = cfDict.get('APP', "basic")
         self.EXEC = cfDict['EXEC']
         self.MODEL_DIR = cfDict['MODEL_DIR']
 
